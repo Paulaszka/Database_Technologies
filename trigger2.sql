3259179fa7c19a7aad0a90625aec45158ab87f88
Paulaszka <pkropkasz@gmail.com>
2026-03-23 21:00:50 +0100

    one trigger implemented

--- trigger2.sql
+++ trigger2.sql
@@ -1,29 +1,28 @@
+-- Wyzwalacz AFTER DELETE na tabeli locations.
+-- Wykorzystuje kursor, aby pobrać i wypisać informację o usuniętych miastach oraz przypisanych do nich krajach.
+
 CREATE OR ALTER TRIGGER delete_trigger ON locations
 AFTER DELETE
 AS
 BEGIN
-	-- Zmienne do przechowywania wyników kursora
-	DECLARE @city NVARCHAR(255);
-	DECLARE @country_name NVARCHAR(100);
+	DECLARE @v_city NVARCHAR(255);
+	DECLARE @v_country_name NVARCHAR(100);
 
-	-- Deklaracja kursora
-	DECLARE  location_cursor CURSOR FOR
+	DECLARE location_cursor CURSOR FOR
 	SELECT c.country_name, d.city
 	FROM DELETED d
 	JOIN countries c on c.country_id = d.country_id;
 
-	-- Otwarcie kursora
 	OPEN location_cursor;
 
-	-- Pobieranie danych z kursora i wyœwietlanie wyników
-	FETCH NEXT FROM location_cursor INTO @country_name, @city;
+	FETCH NEXT FROM location_cursor INTO @v_country_name, @v_city;
 
 	WHILE @@FETCH_STATUS = 0
 	BEGIN
-		PRINT 'Usunieto miasto: ' + @city + ' w kraju: ' + @country_name;
-		FETCH NEXT FROM location_cursor INTO @country_name, @city;
+		PRINT 'Deleted city: ' + @v_city + ' in country: ' + @v_country_name;
+		FETCH NEXT FROM location_cursor INTO @v_country_name, @v_city;
 	END;
--- Zamkniêcie kursora i zwolnienie zasobów
+
 CLOSE location_cursor;
 DEALLOCATE location_cursor;
 END;
