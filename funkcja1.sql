3b027b46ca575b58e0ef63aa72d1a1ff9c166fc4
Paulaszka <pkropkasz@gmail.com>
2026-03-24 17:27:39 +0100

    last lunction implemented

--- funkcja1.sql
+++ funkcja1.sql
@@ -1,16 +1,43 @@
-CREATE OR ALTER FUNCTION func4 (@input NVARCHAR(100))
-RETURNS decimal(5,2)
-AS BEGIN
-	DECLARE @deparmentCount decimal
-	DECLARE @sumEmployes decimal
+-- Funkcja zwracająca statystyki
+-- departamentów dla podanej lokalizacji (@p_location_id).
+-- Zwraca nazwę departamentu, liczbę pracowników oraz procentowy udział 
+-- pracowników departamentu w całkowitej liczbie pracowników danej lokalizacji.
 
-	SELECT @deparmentCount = count(*) FROM employees
-	WHERE employees.department_id = (SELECT departments.department_id FROM departments
-									WHERE departments.department_name = @Input)
+CREATE OR ALTER FUNCTION departments_in_location_stats (
+    @p_location_id NUMERIC
+)
+RETURNS TABLE
+AS
+RETURN (
+    WITH employees_in_deperatments AS (
 
-	SELECT @sumEmployes = count(*) FROM employees
+        SELECT 
+            d.department_name,
+            COUNT(e.employee_id) AS employee_count
+        FROM departments d
+        LEFT JOIN employees e ON d.department_id = e.department_id
+        WHERE d.location_id = @p_location_id
+        GROUP BY d.department_name
+    ),
+    employees_in_location_sum AS (
+        SELECT SUM(employee_count) AS total_count
+        FROM employees_in_deperatments
+    )
 
-    RETURN ROUND((@deparmentCount / @sumEmployes) * 100, 2)
-END
+    SELECT
+        dc.department_name,
+        dc.employee_count,
+        CAST(
+            CASE 
+                WHEN eil.total_count = 0 THEN 0
+                ELSE (CAST(dc.employee_count AS DECIMAL(18,4)) / eil.total_count) * 100
+            END 
+        AS DECIMAL(10,2)) AS percentage_of_location
+    FROM employees_in_deperatments dc
+    CROSS JOIN employees_in_location_sum eil
+    ORDER BY percentage_of_location DESC
+);
 
-SELECT department_id, department_name, dbo.func4(department_name) AS PERCENTAGE FROM departments
+
+-- Przykład wywołania:
+SELECT * FROM departments_in_location_stats (1700) ORDER BY percentage_of_location DESC;
